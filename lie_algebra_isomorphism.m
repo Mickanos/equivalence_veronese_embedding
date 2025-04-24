df3fb1a6ec627dcb96a7faa32595aebaa9fe86bd
Mickanos <62935178+Mickanos@users.noreply.github.com>
2025-04-24 14:33:38 +0300

    Corrected EnvelopingAlgebra in the case that the Cartan subalgebra is already split.

--- lie_algebra_isomorphism.m
+++ lie_algebra_isomorphism.m
@@ -64,13 +64,15 @@ end function;
 
 //Produces Lie algebras gln and sln, with a projection and a lifting map.
 gln_sln := function(k, n)
-  gln := MatrixLieAlgebra(k, n);
-  L, phi := LieAlgebra(gln);
-  sln, proj := quo<L | Basis(Center(L))>;
-  lifts := [M - (Trace(M)/n) * One(gln) where M is S @@ (phi * proj) :
-    S in Basis(sln)];
-  lift_map := hom< sln -> L | lifts @ phi>;
-  return gln, sln, phi * proj, lift_map * Inverse(phi);
+  Mat := MatrixAlgebra(k, n);
+  A, phi := Algebra(Mat);
+  gln, psi := LieAlgebra(A);
+  mat_to_gln := phi * psi;
+  sln, proj := quo<gln | Basis(Center(gln))>;
+  lifts := [(M - (Trace(M)/n) * One(Mat)) @ mat_to_gln
+    where M is S @@ (mat_to_gln * proj) : S in Basis(sln)];
+  lift_map := hom< sln -> gln| lifts>;
+  return gln, sln, mat_to_gln, lift_map;
 end function;
 
 //Given a Lie algebra isomorphic to some sl_n(k), computes an enveloping
@@ -79,36 +81,34 @@ EnvelopingAlgebra := function(L)
   K, LK, HK, bc_map := BaseChangeAndSplitCartan(L);
   NL, Liso := NormaliseSplitLieAlgebra(LK, HK);
   _, n := IsSquare(Dimension(L) + 1);
-  gln, sln, _, lift_sln := gln_sln(K, n);
+  gln, sln, mat_to_gln, sln_to_gln := gln_sln(K, n);
   D := SplittingCartanSubalgebra(sln);
   NM, Miso := NormaliseSplitLieAlgebra(sln, D);
   iiso := hom < NL -> NM | Basis(NM)>;
   lie_iso := Liso * iiso * Inverse(Miso);
   assert IsIsomorphism(lie_iso);
-  rho := bc_map * lie_iso * lift_sln;
+  rho := bc_map * lie_iso * sln_to_gln;
   if K eq BaseRing(L) then
-    A := MatrixAlgebra(K, n);
-    L, phi := LieAlgebra(A);
-    psi := hom<L -> gln | Basis(gln)>;
-    assert IsIsomorphism(psi);
+    A, phi := Algebra(Domain(mat_to_gln));
     print "The Cartan subalgebra was split in the base field.";
-    return A, gln, phi * psi, rho;
+    return A, Inverse(phi) * mat_to_gln, rho;
   end if;
   k := BaseField(L);
   d := Degree(K,k);
-  AssocBasis := [MatrixRestrictionOfScalars(k, b @ rho): b in Basis(L)];
+  AssocBasis := [MatrixRestrictionOfScalars(k, b @ (rho * Inverse(mat_to_gln))):
+    b in Basis(L)];
   Mdn := MatrixAlgebra(k, d*n);
   A, psi := Algebra(sub<Mdn | AssocBasis>);
   A_Lie, phi := LieAlgebra(A);
   env := hom< L -> A_Lie | [b @ (psi * phi) : b in AssocBasis]>;
-  return A, A_Lie, phi, env;
+  return A, phi, env;
 end function;
 
 //Outputs an isomorphism from the Lie algebra L to sln.
 //Technically, the map just gives images as matrices, not as element of
 //a Lie algebra. This is more convenient for our purposes.
 SplitSln := function(L)
-  A, A_Lie, phi, env := EnvelopingAlgebra(L);
+  A, phi, env := EnvelopingAlgebra(L);
   psi := SplitMatrixAlgebra(A);
   return env * Inverse(phi) * psi;
 end function;
