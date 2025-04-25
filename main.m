a7bdd151e789452e4ea333cfafed5202fb57adf4
Mickanos <62935178+Mickanos@users.noreply.github.com>
2025-04-25 10:41:30 +0300

    cleaned main file

--- main.m
+++ main.m
@@ -2,8 +2,3 @@ load "utility.m";
 load "gen.m";
 load "lie_algebra_isomorphism.m";
 load "projective_equivalence.m";
-
-eqs := GenToyVeronese();
-n := 4;
-d := 2;
-sol, pairs := EquivalenceToVeronese(n, d, eqs);
