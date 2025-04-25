f147e7ded5bcfda91f27c190aaddf39943000043
Mickanos <62935178+Mickanos@users.noreply.github.com>
2025-04-25 14:48:55 +0300

    Added RoutineTest procedure for convenience

--- projective_equivalence.m
+++ projective_equivalence.m
@@ -79,3 +79,22 @@ EquivalenceToVeronese := function(n, d, eqs)
     lie_isom := VeroneseLieAlgebraIsom(n, d, eqs);
     return LieAlgebraRepresentationIsomorphism(lie_isom);
 end function;
+
+RoutineTest := procedure(p, d)
+    n := 4;
+    k := 2;
+    print "Time to generate equations:";
+    time eqs := GenVeronese(p, d, k);
+    print "Time to look for a projective equivalence:";
+    time sol := EquivalenceToVeronese(n, d, eqs);
+    if IsEmpty(sol) then
+        print "The isomorphism of Lie algebras did not yield a projective",  
+        " equivalence.";
+    else
+        if CheckEquivalenceToVeronese(eqs, sol[1], n, d) then
+            print "An equivalence was found.";
+        else
+            print "The program did output an incorrect solution.";
+        end if;
+    end if;
+end procedure;
