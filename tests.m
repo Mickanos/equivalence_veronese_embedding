774eb891f1a22436ad2acbd9b9f02ec2f04a37f7
Mickanos <62935178+Mickanos@users.noreply.github.com>
2025-04-25 08:23:03 +0300

    Added test for EquivalenceToVeronese

--- tests.m
+++ tests.m
@@ -35,6 +35,27 @@ TestLieAlgebraVeroneseEmbedding := procedure()
     assert IsLieHom(e, Basis(Domain(e)));
 end procedure;
 
+TestEquivalenceToVeronese := procedure()
+    eqs := GenToyVeronese();
+    n := 4;
+    d := 2;
+    sol, pairs := EquivalenceToVeronese(n, d, eqs);
+    assert #sol gt 0;
+    k := BaseRing(pairs[1][1]);
+    gl := MatrixLieAlgebra(k, 10);
+    A := sub<gl | [p[1] : p in pairs]>;
+    L, phi := LieAlgebra(A);
+    B := sub<gl | [p[2] : p in pairs]>;
+    M, psi := LieAlgebra(B);
+    L := ChangeBasis(L, [p[1] @ phi : p in pairs]);
+    iso := hom<L -> M | [p[2] @ psi : p in pairs]>;
+    assert IsIsomorphism(iso);
+    T := Matrix(k, 10, 10, Eltseq(sol[1]));
+    iT := T^-1;
+    assert &and[T * p[1] * iT eq p[2] : p in pairs];
+end procedure;
+
 TestSplitSln();
 TestSplitSlnSplitCartan();
 TestLieAlgebraVeroneseEmbedding();
+TestEquivalenceToVeronese();
